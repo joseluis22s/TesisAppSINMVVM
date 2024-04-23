585daff31b79603b6d2ef7ceb421d6f10cea9457
PC_Jose <jose.luis22_@hotmail.es>
2024-04-23 02:30:14 -0500

    Crear cuenta 70%. Icono toast, BD

--- Views/CrearNuevaCuentaPage.xaml.cs
+++ Views/CrearNuevaCuentaPage.xaml.cs
@@ -21,11 +21,11 @@ public partial class CrearNuevaCuentaPage : ContentPage
     // EVENTOS
     private async void Button_SiguienteCrear_Clicked(object sender, EventArgs e)
     {
-        if (enEjecucion)
-        {
-            return;
-        }
-        enEjecucion = true;
+        //if (enEjecucion)
+        //{
+        //    return;
+        //}
+        //enEjecucion = true;
         if (Button_SiguienteCrear.Text == "Siguiente")
         {
             bool esUsuarioValido = await VerificarNuevoUsuarioAsync();
@@ -47,18 +47,26 @@ public partial class CrearNuevaCuentaPage : ContentPage
         {
             bool esContrasenaValida = await VerificarNuevaContrasenaAsync();
             bool sonContrasenaIguales = await VerificarNuevaContrasenaIgualAsync();
-            bool tieneParecidoUsuarioContrasena = await VerficarParecidoUsuarioContrasenaAsync();
-            if (esContrasenaValida || sonContrasenaIguales || !tieneParecidoUsuarioContrasena)
+            bool tieneParecidoUsuarioContrasena = await VerificarParecidoUsuarioContrasenaAsync();
+            if (esContrasenaValida && sonContrasenaIguales && !tieneParecidoUsuarioContrasena)
             {
                 await Toast.Make("Cuenta creada").Show();
                 await Navigation.PopAsync();
             }
+            else
+            {
+                await Toast.Make("Todos los campos deben ser válidos").Show();
+            }
         }
-        enEjecucion = false;
+        //enEjecucion = false;
     }
     private async void Image_CancelClose_TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
     {
-        await CrearNuevaCuentaPagePopAsync();
+        bool respuesta = await DisplayAlert("Alerta", "¿Desea regresar? Perderá el progreso realizado", "Confirmar", "Cancelar");
+        if (respuesta)
+        {
+            await CrearNuevaCuentaPagePopAsync();
+        }
     }
     private void Image_BackIcon_TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
     {
@@ -101,6 +109,78 @@ public partial class CrearNuevaCuentaPage : ContentPage
             Image_CorreoUncheckIcon.IsVisible = true;
         }
     }
+    private async void Entry_NuevaContrasena_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        bool esContrasenavalida = await VerificarNuevaContrasenaAsync();
+        bool esParecidoUsuarioContrasena = await VerificarParecidoUsuarioContrasenaAsync();
+        string contrasenaIgual = Entry_ContrasenaIgual.Text;
+        if (!string.IsNullOrEmpty(contrasenaIgual))
+        {
+            bool esIgualContrasena = await VerificarNuevaContrasenaIgualAsync();
+            if (esIgualContrasena)
+            {
+                Label_ValidacionContrasenaIgual.IsVisible = false;
+                Image_ContrasenaIgualCheckIcon.IsVisible = true;
+                Image_ContrasenaIgualUncheckIcon.IsVisible = false;
+            }
+            else
+            {
+                Label_ValidacionContrasenaIgual.IsVisible = true;
+                Image_ContrasenaIgualCheckIcon.IsVisible = false;
+                Image_ContrasenaIgualUncheckIcon.IsVisible = true;
+            }
+        }
+        if (esContrasenavalida)
+        {
+            Label_ValidacionNuevaContrasena1.IsVisible = false;
+            if (esParecidoUsuarioContrasena)
+            {
+                Label_ValidacionNuevaContrasena2.IsVisible = true;
+                Image_NuevaContrasenaCheckIcon.IsVisible = false;
+                Image_NuevaContrasenaUncheckIcon.IsVisible = true;
+            }
+            else
+            {
+                Label_ValidacionNuevaContrasena2.IsVisible = false;
+                Image_NuevaContrasenaCheckIcon.IsVisible = true;
+                Image_NuevaContrasenaUncheckIcon.IsVisible = false;
+            }
+        }
+        else
+        {
+            Label_ValidacionNuevaContrasena1.IsVisible = true;
+            Image_NuevaContrasenaCheckIcon.IsVisible = false;
+            Image_NuevaContrasenaUncheckIcon.IsVisible = true;
+            if (esParecidoUsuarioContrasena)
+            {
+                Label_ValidacionNuevaContrasena2.IsVisible = true;
+            }
+            else
+            {
+                Label_ValidacionNuevaContrasena2.IsVisible = false;
+            }
+        }
+
+
+    }
+    private async void Entry_ContrasenaIgual_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        bool esIgual = await VerificarNuevaContrasenaIgualAsync();
+        if (esIgual)
+        {
+            Label_ValidacionContrasenaIgual.IsVisible = false;
+            Image_ContrasenaIgualCheckIcon.IsVisible = true;
+            Image_ContrasenaIgualUncheckIcon.IsVisible = false;
+        }
+        else
+        {
+            Label_ValidacionContrasenaIgual.IsVisible = true;
+            Image_ContrasenaIgualCheckIcon.IsVisible = false;
+            Image_ContrasenaIgualUncheckIcon.IsVisible = true;
+        }
+    }
+
+    
 
     private void Button_BorrarTabla_Clicked(object sender, EventArgs e)
     {
@@ -114,6 +194,11 @@ public partial class CrearNuevaCuentaPage : ContentPage
     private Task<bool> VerificarNuevoUsuarioAsync()
     {
         string nuevoUsuario = Entry_NuevoUsuario.Text;
+        if (string.IsNullOrEmpty(nuevoUsuario))
+        {
+            nuevoUsuario = "";
+        }
+
         if (nuevoUsuario.Length != 0) 
             {
                 bool tieneDigito = false;
@@ -138,18 +223,19 @@ public partial class CrearNuevaCuentaPage : ContentPage
     private Task<bool> VerificarNuevoCorreoAsync()
     {
         string correo = Entry_NuevoCorreo.Text;
+        if (string.IsNullOrEmpty(correo))
+        {
+            correo = "";
+        }
         return Task.FromResult(Regex.IsMatch(correo, @"^(?!.*[\sÀ-ÿ])[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"));
-
-        //bool isValid = Regex.IsMatch(correo, @"^(?!.*[\sÀ-ÿ])[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$");
-        //if (isValid)
-        //{
-        //    return true;
-        //}
-        //return false;
     }
     private Task<bool> VerificarNuevaContrasenaAsync()
     {
         string nuevaContrasena = Entry_NuevaContrasena.Text;
+        if (string.IsNullOrEmpty(nuevaContrasena))
+        {
+            nuevaContrasena = "";
+        }
         if (nuevaContrasena.Length != 0)
         {
             bool tieneDigito = false;
@@ -176,15 +262,28 @@ public partial class CrearNuevaCuentaPage : ContentPage
     {
         string contrasena1 = Entry_NuevaContrasena.Text;
         string contrasena2 = Entry_ContrasenaIgual.Text;
+        if (string.IsNullOrEmpty(contrasena1))
+        {
+            contrasena1 = "";
+        }
+        if (string.IsNullOrEmpty(contrasena2))
+        {
+            contrasena2 = "";
+        }
 
         return Task.FromResult(contrasena1 == contrasena2);
     }
-    private Task<bool> VerficarParecidoUsuarioContrasenaAsync()
+    private Task<bool> VerificarParecidoUsuarioContrasenaAsync()
     {
         string usuario = Entry_NuevoUsuario.Text;
         string contrasena = Entry_NuevaContrasena.Text;
+        if (string.IsNullOrEmpty(contrasena))
+        {
+            contrasena = "";
+        }
         return Task.FromResult((usuario.Equals(contrasena) || contrasena.Contains(usuario)));
     }
+    
 
 
 
