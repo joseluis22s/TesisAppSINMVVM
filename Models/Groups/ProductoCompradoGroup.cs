13ce189656fa0a8d2429b0dc25f3ad07dfa8f3b6
PC_Jose <jose.luis22_@hotmail.es>
2024-07-14 19:22:30 -0500

    antes del inteno de agrupar los datos del histroail de compras

--- Models/Groups/ProductoCompradoGroup.cs
+++ Models/Groups/ProductoCompradoGroup.cs
@@ -5,7 +5,6 @@ namespace TesisAppSINMVVM.Models.Groups
     public class ProductoCompradoGroup : List<Tbl_ProductoComprado>
     {
         public string DiaFecha { get; private set; }
-        public List<ProductoCompradoSubGroup> SubGrupos { get; set; }
 
         public ProductoCompradoGroup(string diaFecha, List<Tbl_ProductoComprado> prodcutoComprado) : base(prodcutoComprado)
         {
